2bf9ffd93ca6ec4f10e29e9f5def71afd88cfcad
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-03-17 12:46:53 +0530

    Created a store for toggling Sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Provider } from "react-redux";
+import store from "./utils/store";
+
 import "./App.css";
 import Head from "./components/Head";
 import Body from "./components/Body";
 
 function App() {
   return (
-    <div>
-      <Head />
-      <Body />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Head />
+        <Body />
+      </div>
+    </Provider>
   );
 }
 
