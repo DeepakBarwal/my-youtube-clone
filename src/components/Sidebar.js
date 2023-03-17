84cb2bbd5126c0a22673967447d76e17c9486986
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-03-17 12:31:39 +0530

    Created Sidebar skeleton

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,28 @@ import MenuItems from "./MenuItems";
 
 const Sidebar = () => {
   return (
-    <div>
-      <MenuItems />
+    <div className="p-5 shadow-lg w-48">
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      {/* <MenuItems /> */}
     </div>
   );
 };
